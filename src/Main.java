553bf6609fe9a3950484b5e47f6cfb0c875d5ba3
Ronny1100 <113380723+Ronny1100@users.noreply.github.com>
2022-11-28 14:37:55 -0500

    started

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,15 @@
+import java.util.*;
 
 public class Main {
 
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		// Operators
+		Math.random();
+		System.out.println(random.nextlnt);
+		System.out.println("Enter a random number");
 	}
 
 }
