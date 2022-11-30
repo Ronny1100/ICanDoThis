1582e791e81828538db5c5fa8cf0298660f7cd94
Ronny1100 <113380723+Ronny1100@users.noreply.github.com>
2022-11-30 14:40:13 -0500

    working on

--- src/Arrays.java
+++ src/Arrays.java
@@ -1,4 +1,46 @@
+import java.util.Scanner;
 
 public class Arrays {
+	
+	private static void sort(int[] list) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		
+		// Ask
+		int userChoice;
+		System.out.println("What size of array would you like?");
+		
+		// Minimum
+		int[] array = {6,5,2,8,9,7,4,1,3};
+		int min = array[0];
+		for (int index = 0; index < array.length; index++)
+			if (array[index] < min);
+				min = array[index];
+		System.out.println("The minimum value is" + min);
+		
+		// Average
+		double scores[] = new double[10];
+		double sum = 0.0, avg = 0.0;
+		Scanner keyboard = new Scanner(System.in);
+		
+		System.out.println();
+		for(int i = 0; i < scores.length; i++) {
+			scores[i] = keyboard.nextInt();
+		}
+		System.out.println();
+		for(int i = 0; i < scores.length; i++) {
+			System.out.println(scores[i]);
+		}
+		for (int i = 0; i < scores.length; i++) {
+			sum = sum + scores[i];
+			avg = sum / scores.length;
+		}
+		System.out.println("The average of your value is" + avg);
+			
+			
+		}
+	}
 
-}
+//int[] list1= {6,5,2,8,9,7,4,1,3};
+//Arrays.sort (list1);
+//for (int i: list1);
--- src/Operators.java
+++ src/Operators.java
@@ -6,12 +6,20 @@ public class Operators {
 		Scanner in = new Scanner(System.in);
 		int userChoice;
 		do {
-			Math.random();
-			System.out.println(random.nextlnt);
+			
+			int x = 1;
+			int y = 3;
+			int r = 12;
+			int u = 5;
+			
 			System.out.println("Enter a random number");
-			double answer = (userChoice - 1);
-			double number = (userChoice * 3);
-		
+			int answer = (userChoice - x);
+			int sum1 = (answer * y);
+			int sum2= (sum1 + r);
+			int sum3 = (sum2 / y);
+			int sum4= (answer + u);
+			int = (answer - sum);
+			System.out.println("The answer is" + answer);
 		}
 	}
 }
