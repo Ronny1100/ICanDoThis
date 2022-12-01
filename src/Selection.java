5affd186c3362365e1e95872afcaf9d68089cd2e
Ronny1100 <113380723+Ronny1100@users.noreply.github.com>
2022-12-01 14:41:18 -0500

    working on selection

--- src/Selection.java
+++ src/Selection.java
@@ -1,4 +1,48 @@
-
+import java.util.Scanner;
 public class Selection {
+	
 
+	public static void main (Scanner [] args) {
+		
+	}
+		Scanner in = new Scanner(System.in);
+		
 }
+//Tax = (0.15*49,020) + (0.205*(98040-49020)) + (0.26*(151978-98040)) + (0.29*(216511-151978)) + (0.33*(220000-216511)) = 7353 + 10049.10 + 14023.88 + 18714.57 + 1151.37 = $51,291.92
+
+		random = new random();
+		System.out.println("Enter a $ value");
+		
+		//15% on the first $49,020
+		double tax1 = 0.15; {
+			tax1 = (49020) * 0.15;
+			System.out.println("Total tax is" + sum1);
+		
+		//20.5% on the portion over $49,020 up to $98,040
+		double tax2 = 0.205; {
+			tax2 = (98040-49020) * 0.205;
+			System.out.println("Total tax is" + sum2);
+	
+	}
+		//26% on the portion over $98,040 up to $151,978
+		double tax3 = 0.26; {
+			tax3 = (151978-98040)*0.26;
+			System.out.println("Total tax is" + sum3);
+		
+	}
+		//29% on the portion over $151,978 up to $216,511
+		double tax4 = 0.29; {
+			tax4 = (216511-151978)*0.29;
+			System.out.println("Total tax is" + sum4);
+		}
+	}
+}
+		//33% on the portion over $216,511
+		double tax5 = 0.33; {
+			tax5 = (216511) * 0.33;
+			System.out.println("Total tax is" + sum5);
+		
+	}
+}
+		
+	
